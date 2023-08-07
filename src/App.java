9b142acde85409889f6897e2a2bca805a59e7dad
albertahnlide <albert.ahnlide@outlook.com>
2023-08-07 10:53:04 +0200

    Test till en början

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+    List<Node<Integer>> list;
+    public App(){
+        list = new ArrayList<Node<Integer>>();
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
     }
+
+
+    public class Node<E>{
+
+        public E data;
+        public Node<E> next; 
+
+        public Node(E e){
+            data = e;
+            next = null;
+        }
+
+
+    }
 }
+
